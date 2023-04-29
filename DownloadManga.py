075e697888c23bb4c94d4f7dabfa60e304108e4c
Arthur Fernandes <arthur0139@gmail.com>
2023-04-29 12:57:56 -0300

    minor adjustments

--- DownloadManga.py
+++ DownloadManga.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 import requests
+import selenium.common
 from tqdm import tqdm
-from dataclasses import dataclass
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -10,16 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.remote.webelement import WebElement
-
-
-@dataclass
-class DownloadMangaDataclass:
-    URL: str = "https://po2scans.com"
-    USER_FOLDER: str = os.path.expanduser('~')
-    CHROMEDRIVER: str = os.path.join(os.path.dirname(
-        os.path.abspath(__file__)), "chromedriver")
-    LOCAL_DOWNLOADS: str = os.path.join(os.getcwd(), 'downloads')
-    CHAPTERS_DESTINATION: str = os.path.join(USER_FOLDER, 'Documentos', 'mangás')
+from DownloadMangaDataclass import DownloadMangaDataclass
 
 
 class DownloadManga:
@@ -46,9 +37,11 @@ class DownloadManga:
         self.move_files_to_destination()
 
     def move_files_to_destination(self) -> None:
-
-        shutil.make_archive(self.local_downloads_chapter, 'zip', self.local_downloads_chapter)
-        shutil.move(self.local_downloads_chapter + '.zip', DownloadMangaDataclass.CHAPTERS_DESTINATION)
+        try:
+            shutil.make_archive(self.local_downloads_chapter, 'zip', self.local_downloads_chapter)
+            shutil.move(self.local_downloads_chapter + '.zip', DownloadMangaDataclass.CHAPTERS_DESTINATION)
+        except FileNotFoundError or FileExistsError as e:
+            print("Erro ao mover para os paths: {0}".format(e.args[0]))
 
     def download_files(self) -> None:
         pbar = tqdm(enumerate(self.images_element), desc='Baixando arquivos',
@@ -59,45 +52,66 @@ class DownloadManga:
             try:
                 res = requests.get(src, allow_redirects=True)
             except requests.exceptions.RequestException as e:
-                print(e.args)
+                print("Erro ao fazer os downloads: {0}".format(e.args[0]))
                 exit()
             file = open(os.path.join(self.local_downloads_chapter, str(index) + '.png'), 'wb')
             file.write(res.content)
             file.close()
 
     def _handle_paths(self) -> None:
-        if not os.path.isdir(DownloadMangaDataclass.LOCAL_DOWNLOADS):
-            os.mkdir(DownloadMangaDataclass.LOCAL_DOWNLOADS)
+        try:
+            if not os.path.isdir(DownloadMangaDataclass.LOCAL_DOWNLOADS):
+                os.mkdir(DownloadMangaDataclass.LOCAL_DOWNLOADS)
 
-        if os.path.isdir(self.local_downloads_chapter):
-            shutil.rmtree(self.local_downloads_chapter)
+            if os.path.isdir(self.local_downloads_chapter):
+                shutil.rmtree(self.local_downloads_chapter)
 
-        os.mkdir(self.local_downloads_chapter)
+            os.mkdir(self.local_downloads_chapter)
 
-        if os.path.isfile(self.chapters_destination_file):
-            os.remove(self.chapters_destination_file)
+            if os.path.isfile(self.chapters_destination_file):
+                os.remove(self.chapters_destination_file)
+        except FileNotFoundError or FileExistsError as e:
+            print("Erro ao obter os paths: {0}".format(e.args[0]))
+            exit()
 
     def _set_variables(self) -> None:
-        self.title_text = self.title_element.accessible_name + " - " + self.chapter_element.text.split('-')[0].strip()
+        try:
+            self.title_text = self.title_element.accessible_name + " - " + \
+                              self.chapter_element.text.split('-')[0].strip()
 
-        self.local_downloads_chapter = os.path.join(DownloadMangaDataclass.LOCAL_DOWNLOADS, self.title_text)
-        self.chapters_destination_file = os.path.join(DownloadMangaDataclass.CHAPTERS_DESTINATION,
-                                                      self.title_text + '.zip')
+            self.local_downloads_chapter = os.path.join(DownloadMangaDataclass.LOCAL_DOWNLOADS, self.title_text)
+            self.chapters_destination_file = os.path.join(DownloadMangaDataclass.CHAPTERS_DESTINATION,
+                                                          self.title_text + '.zip')
+        except FileNotFoundError or FileExistsError as e:
+            print("Erro ao obter os paths: {0}".format(e.args[0]))
+            exit()
 
     def _get_elements(self) -> None:
-        self.chapter_element: WebElement = self.wait.until(ec.presence_of_element_located(
-            (By.XPATH, "//*[@class='chap-select']/option[@selected]")))
-        self.images_element: list[WebElement] = self.wait.until(ec.presence_of_all_elements_located(
-            (By.XPATH, "//*[@data-hash[contains(., 'page=')]]/img")))
-        self.title_element: WebElement = self.wait.until(ec.presence_of_element_located(
-            (By.XPATH, "//*[@class='series-title']")))
+        try:
+            self.chapter_element: WebElement = self.wait.until(ec.presence_of_element_located(
+                (By.XPATH, "//*[@class='chap-select']/option[@selected]")))
+            print("Capítulo obtido")
+            self.images_element: list[WebElement] = self.wait.until(ec.presence_of_all_elements_located(
+                (By.XPATH, "//*[@data-hash[contains(., 'page=')]]/img")))
+            print("Imagens obtidas")
+            self.title_element: WebElement = self.wait.until(ec.presence_of_element_located(
+                (By.XPATH, "//*[@class='series-title']")))
+            print("Título obtido")
+        except selenium.common.exceptions.ElementNotVisibleException as e:
+            print("Ocorreu um erro ao obter os elementos html: {0}".format(e.args[0]))
+            exit()
 
     def _setup_selenium(self, chapter_code: str) -> None:
-        options = Options()
-        # options.add_argument('--headless')
-        service: Service = Service(executable_path=DownloadMangaDataclass.CHROMEDRIVER)
-        driver: Chrome = Chrome(service=service, options=options)
-        driver.get(DownloadMangaDataclass.URL + '/reader/' + chapter_code)
-        driver.fullscreen_window()
-        driver.execute_script("document.documentElement.requestFullscreen();")
-        self.wait = WebDriverWait(driver, 10)
+        try:
+            options = Options()
+            # options.add_argument('--headless')
+            service: Service = Service(executable_path=DownloadMangaDataclass.CHROMEDRIVER)
+            driver: Chrome = Chrome(service=service, options=options)
+            driver.get(DownloadMangaDataclass.URL + '/reader/' + chapter_code)
+            driver.fullscreen_window()
+            driver.execute_script("document.documentElement.requestFullscreen();")
+            print("Setup finalizado")
+            self.wait = WebDriverWait(driver, 10)
+        except selenium.common.exceptions.WebDriverException as e:
+            print("Setup do Selenium falhou: {0}".format(e.args[0]))
+            exit()
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     chapter_code: str = str(input("Digite o código do capítulo:\n"))
     downloadManga = DownloadManga(chapter_code=chapter_code)
     downloadManga.execute()
-    print("Download de {}".format(downloadManga))
+    print("Download de {0} completo".format(downloadManga))
